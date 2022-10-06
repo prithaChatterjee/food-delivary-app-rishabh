26325bf54b52688035fb05d2998724b00a3581a0
prithaChatterjee <pchatterjee518@gmail.com>
2022-10-06 19:11:15 +0530

    updates for today

--- src/molecules/Dishes/index.js
+++ src/molecules/Dishes/index.js
@@ -2,19 +2,25 @@ import React from 'react'
 import Rating from '../../Atoms/Rating'
 import { BsFillRecordCircleFill } from "react-icons/bs";
 import { StyleDishes } from './style.dishes';
+import AddToCart from '../../Atoms/AddToCart';
 
 const Dish = (props) => {
-    const {description, name, price, rating} = props
+    const { description, name, price, rating } = props
     return (
         <StyleDishes>
-            <div className="numbering">
-                <BsFillRecordCircleFill />
+            <div>
+                <div className="numbering">
+                    <BsFillRecordCircleFill />
+                </div>
+                <div className="itemDetails">
+                    <h3>{name}</h3>
+                    <p>${price}</p>
+                    <Rating rate={rating} />
+                    <div>{description}</div>
+                </div>
             </div>
-            <div className="itemDetails">
-                <h3>{name}</h3>
-                <p>${price}</p>
-                <Rating rate={rating} />
-                <div>{description}</div>
+            <div>
+                <AddToCart number={0}/>
             </div>
         </StyleDishes>
     )
--- src/molecules/Dishes/style.dishes.js
+++ src/molecules/Dishes/style.dishes.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 export const StyleDishes = styled.div`
-margin-bottom: 1rem;
     display: flex;
-    gap: 10px;
-    & > div.itemDetails h3, & > div.itemDetails p, & > div.itemDetails div{
-        margin-bottom: 0.375rem
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    & > div{
+        display: flex;
+        gap: 10px;
+        align-items: flex-start;
+        & > div.itemDetails h3, & > div.itemDetails p, & > div.itemDetails div{
+            margin-bottom: 0.375rem
+        }
     }
 `
